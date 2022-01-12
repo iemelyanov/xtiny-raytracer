a4bec2fe047ceade87a0e150ea4b5ebe8250843e
Igor Emelianov <cybercjp@gmail.com>
2022-01-12 15:55:53 +0200

    add simple parallel renderer

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 build:
-	clang -std=c17 main.c camera.c hitablelist.c material.c sphere.c -Wall -O2 -o xtiny-raytracer
+	clang -std=c17 main.c camera.c hitablelist.c material.c sphere.c -lpthread -Wall -O2 -o xtiny-raytracer
 
 debug:
-	clang main.c camera.c hitablelist.c material.c sphere.c -lm -Wall -g -fsanitize=address -o xtiny-raytracer -D DEBUG
+	clang main.c camera.c hitablelist.c material.c sphere.c -lpthread -lm -Wall -g -fsanitize=address -o xtiny-raytracer -D DEBUG
 
 render:
 	./xtiny-raytracer > image.ppm
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdatomic.h>
+#include <pthread.h>
 #include "xtiny_raytracer.h"
 
 #define N 500
@@ -28,8 +28,7 @@ static vec3 color(const ray *r, const hitableList *world, int32_t depth) {
     if (hitableListHit(world, r, 0.001, FLT_MAX, &rec)) {
         ray scattered = {(vec3){0, 0, 0}, (vec3){0, 0, 0}};
         vec3 attenuation = {0, 0, 0};
-        float q = rec.material->scatter(
-            rec.material, r, &rec, &attenuation, &scattered);
+        float q = rec.material->scatter(rec.material, r, &rec, &attenuation, &scattered);
         if (depth < 50 && q)
            return vec3Mul(attenuation, color(&scattered, world, depth + 1));
         return (vec3){0, 0, 0};
@@ -100,10 +99,50 @@ uint32_t numDigits(uint32_t n) {
     return l;
 }
 
+typedef struct {
+    uint8_t r, g, b;
+} rgb;
+
+typedef struct {
+    uint32_t ny_a, ny_b, ny, nx;
+    uint32_t ns;
+    camera *cam;
+    hitableList *world;
+    rgb *result_bucket;
+} task;
+
+void *render(void *v) {
+    task *t = (task*)v;
+
+    int k = 0;
+    for (uint32_t j = t->ny_a; j > t->ny_b; j--) {
+        for (uint32_t i = 0; i < t->nx; i++) {
+            vec3 col = {0, 0, 0};
+            for (uint32_t s = 0; s < t->ns; s++) {
+                float u = (float)(i + drand48()) / (float)t->nx;
+                float v = (float)(j + drand48()) / (float)t->ny;
+                ray r = cameraGetRay(t->cam, u, v);
+                col = vec3Add(col, color(&r, t->world, 0));
+            }
+
+            col = vec3DivScalar(col, (float)t->ns);
+            col = (vec3){sqrtf(col.x), sqrtf(col.y), sqrtf(col.z)};
+
+            t->result_bucket[k++] = (rgb){
+                .r = (uint32_t)255.99 * col.x,
+                .g = (uint32_t)255.99 * col.y,
+                .b = (uint32_t)255.99 * col.z,
+            };
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
 int main(int argc, char **argv) {
-    const uint32_t nx = 320;
-    const uint32_t ny = 240;
-    const uint32_t ns = 100;
+    const uint32_t nx = 800;
+    const uint32_t ny = 600;
+    const uint32_t ns = 200;
 
     hitable **list = NULL;
     size_t n = randomScene(&list);
@@ -121,26 +160,45 @@ int main(int argc, char **argv) {
     camera cam;
     cameraInit(&cam, lookfrom, lookat, (vec3){0, 1, 0}, 24, (float)nx / (float)ny, aperture, dist_to_focus);
 
+    const int num_threads = 4;
+    const int bucket_size = ny / num_threads * nx;
+    rgb pixels[num_threads][bucket_size];
+    task tasks[num_threads] = { 0 };
 
-    printf("P3\n%d %d\n255\n", nx, ny);
-    for (uint32_t j = ny; j > 0; j--) {
-        for (uint32_t i = 0; i < nx; i++) {
-            vec3 col = {0, 0, 0};
-            for (uint32_t s = 0; s < ns; s++) {
-                float u = (float)(i + drand48()) / (float)nx;
-                float v = (float)(j + drand48()) / (float)ny;
-                ray r = cameraGetRay(&cam, u, v);
-                col = vec3Add(col, color(&r, &world, 0));
-            }
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-            col = vec3DivScalar(col, (float)ns);
-            col = (vec3){sqrtf(col.x), sqrtf(col.y), sqrtf(col.z)};
+    pthread_t threads[num_threads];
+    uint32_t ny_diff = ny / num_threads;
+    uint32_t ny_a = ny, ny_b = ny - ny_diff;
+    for (int i = 0; i < num_threads; i++) {
+        tasks[i] = (task){
+            .ny_a = ny_a,
+            .ny_b = ny_b,
+            .nx = nx,
+            .ny = ny,
+            .ns = ns,
+            .cam = &cam,
+            .world = &world,
+            .result_bucket = pixels[i],
+        };
+        int rc = pthread_create(&threads[i], NULL, render, (void *)(&tasks[i]));
+        if (rc) {
+            fprintf(stderr, "ERROR: return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+        ny_a = ny_b;
+        ny_b -= ny_diff;
+    }
 
-            int ir = (uint32_t)255.99 * col.x;
-            int ig = (uint32_t)255.99 * col.y;
-            int ib = (uint32_t)255.99 * col.z;
-
-            printf("%d %d %d\n", ir, ig, ib);
+    void *status;
+    pthread_attr_destroy(&attr);
+    for (int i = 0; i < num_threads; i++) {
+        int rc = pthread_join(threads[i], &status);
+        if (rc) {
+            fprintf(stderr, "ERROR: return code from pthread_join() is %d\n", rc);
+            exit(-1);
         }
     }
 
@@ -148,9 +206,17 @@ int main(int argc, char **argv) {
         list[i]->destroy(list[i]);
     xfree(list);
 
+    printf("P3\n%d %d\n255\n", nx, ny);
+    for (int i = 0; i < num_threads; i++) {
+        for (int j = 0; j < bucket_size; j++) {
+            rgb c = pixels[i][j];
+            printf("%d %d %d\n", c.r, c.g, c.b);
+        }
+    }
+
     #ifdef DEBUG
     fprintf(stderr, "alloc: %d dealloc: %d\n", total_alloc, total_dealloc);
     #endif
 
-    return 0;
+    pthread_exit(NULL);
 }
